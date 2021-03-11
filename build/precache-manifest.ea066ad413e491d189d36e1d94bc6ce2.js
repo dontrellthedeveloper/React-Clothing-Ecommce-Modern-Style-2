365bfc4b69e67cffc213cdd8a1cf013000784cf4
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-03-10 21:10:50 -0800

    initial commit for homepage

---
+++ build/precache-manifest.ea066ad413e491d189d36e1d94bc6ce2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c9b2c73fcd51bd00548",
+    "url": "/react/molla/demo-5/static/css/8.104cb44e.chunk.css"
+  },
+  {
+    "revision": "35a6c134dfcd6ab52b49",
+    "url": "/react/molla/demo-5/static/js/0.35a6c134.chunk.js"
+  },
+  {
+    "revision": "dfd70abf8e985ac9fd70",
+    "url": "/react/molla/demo-5/static/js/2.dfd70abf.chunk.js"
+  },
+  {
+    "revision": "a13db25fc87752a3b7c4",
+    "url": "/react/molla/demo-5/static/js/3.a13db25f.chunk.js"
+  },
+  {
+    "revision": "1b8404c751af824e2aba",
+    "url": "/react/molla/demo-5/static/js/4.1b8404c7.chunk.js"
+  },
+  {
+    "revision": "28ae0c7e5ae58d48a16f",
+    "url": "/react/molla/demo-5/static/js/main.28ae0c7e.chunk.js"
+  },
+  {
+    "revision": "4ab78cf75d532ebc12e5",
+    "url": "/react/molla/demo-5/static/js/runtime~main.4ab78cf7.js"
+  },
+  {
+    "revision": "9151fc27a529fdafd4ba",
+    "url": "/react/molla/demo-5/static/js/14.9151fc27.chunk.js"
+  },
+  {
+    "revision": "21cdf54564a9462b6127",
+    "url": "/react/molla/demo-5/static/js/7.21cdf545.chunk.js"
+  },
+  {
+    "revision": "9254eae027939078ccf2",
+    "url": "/react/molla/demo-5/static/js/1.9254eae0.chunk.js"
+  },
+  {
+    "revision": "8c9b2c73fcd51bd00548",
+    "url": "/react/molla/demo-5/static/js/8.8c9b2c73.chunk.js"
+  },
+  {
+    "revision": "5d7e2a14ae764e3c6a70",
+    "url": "/react/molla/demo-5/static/js/13.5d7e2a14.chunk.js"
+  },
+  {
+    "revision": "1d3f7c89b3a104ce83ca",
+    "url": "/react/molla/demo-5/static/js/9.1d3f7c89.chunk.js"
+  },
+  {
+    "revision": "3932c9c661338d3d6bd2",
+    "url": "/react/molla/demo-5/static/js/10.3932c9c6.chunk.js"
+  },
+  {
+    "revision": "5ae875f3266dfd909096",
+    "url": "/react/molla/demo-5/static/js/11.5ae875f3.chunk.js"
+  },
+  {
+    "revision": "683f0dc0cd1df1698d66",
+    "url": "/react/molla/demo-5/static/js/12.683f0dc0.chunk.js"
+  },
+  {
+    "revision": "1d3f7c89b3a104ce83ca",
+    "url": "/react/molla/demo-5/static/css/9.62b89595.chunk.css"
+  },
+  {
+    "revision": "21cdf54564a9462b6127",
+    "url": "/react/molla/demo-5/static/css/7.f797737a.chunk.css"
+  },
+  {
+    "revision": "b14679e437be49ccbcc1eaa881aa6204",
+    "url": "/react/molla/demo-5/index.html"
+  }
+];
